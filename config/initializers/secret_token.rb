3e517f3d0cea0c2d7f16630896d9ed6e6bcfc7a6
MonsterCoder <MonsterCoder@gmail.com>
2011-01-14 23:42:12 -0600

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Postmaster::Application.config.secret_token = '453582317135c95fe43e184ffbcc2c7048d40703ffc5e8407d2d0d0d9a7e450e7210f11e9e5391cd4475611d81c1d5dac9f787c08f60ce7c732e0964d2a26843'
