79246912017f3382851fbae73848a830c7db2047
MonsterCoder <MonsterCoder@gmail.com>
2011-01-16 01:46:47 -0600

    fixing home page

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,11 +1,7 @@
 class HomeController < ApplicationController
-  before_filter :authenticate
-  
-  def index
-  end
-  
 
-    def authenticate
-      redirect_to new_user_session_path unless current_user
-    end
+  def index
+    redirect_to new_user_session_path unless current_user
+  end
+
 end
